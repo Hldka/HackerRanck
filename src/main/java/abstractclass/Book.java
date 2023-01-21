4b65b44e7f6d9b9159400359b6895a48dace8306
Hldka <116027001+Hldka@users.noreply.github.com>
2023-01-21 08:39:24 +0100

    first commit

---
+++ src/main/java/abstractclass/Book.java
@@ -0,0 +1,8 @@
+package abstractclass;
+
+public abstract class Book {String title;
+    abstract void setTitle(String s);
+    String getTitle(){
+        return title;
+    }
+}
---
+++ src/main/java/abstractclass/MyBook.java
@@ -0,0 +1,14 @@
+package abstractclass;
+
+public class MyBook {
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    private String title;
+}
