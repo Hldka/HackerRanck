890f39856abe1f4a9fa4230af33b408ec1e56a58
Hldka <116027001+Hldka@users.noreply.github.com>
2023-02-05 17:05:48 +0100

    first commit

---
+++ src/main/java/loops/endoffile.java
@@ -0,0 +1,17 @@
+package loops;
+
+import java.util.Scanner;
+
+public class endoffile {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int line_num=1;
+        while (scan.hasNext()){
+            System.out.println(line_num+""+scan.nextLine());
+            line_num++;
+        }
+        scan.close();
+    }
+}
+/*fatma bizim kiymetlimiz cok sanslisin , ne düsünüyosun falan
+* */
