ed89552651dbdc9d9b3bb2a4f792f8b5d27b1e96
Hldka <116027001+Hldka@users.noreply.github.com>
2023-03-03 11:48:18 +0100

    Merge remote-tracking branch 'origin/main'

---
+++ src/main/java/abstractclass/abstarctclass.java
@@ -0,0 +1,12 @@
+package abstractclass;
+
+import java.util.Scanner;
+
+public class abstarctclass {
+    public static void main(String[] args) {
+
+
+        Scanner scan=new Scanner(System.in);
+
+    }
+}
